c33818f429a1a7fb5d524627cc0dd45b5d4abfc5
Alex Coady <alexandercoady@me.com>
2016-07-12 11:41:09 +0100

    Client state initialised from value rendered on server (hydrates redux)

--- client/index.jsx
+++ client/index.jsx
@@ -4,6 +4,7 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
+import { fromJS } from 'immutable';
 
 // Shared app dependencies
 import routes from './../shared/routes';
@@ -12,8 +13,16 @@ import * as reducers from './../shared/reducers';
 // Import styles
 require('./style/main.scss');
 
+let state = window.__INITIAL_STATE__;
+
+Object
+  .keys(state)
+  .forEach(key => {
+    state[key] = fromJS(state[key]);
+  });
+
 const reducer = combineReducers(reducers);
-const store = createStore(reducer);
+const store = createStore(reducer, state);
 
 render(
   <Provider store={store}>
--- shared/components/projects/reducers.js
+++ shared/components/projects/reducers.js
@@ -5,5 +5,7 @@ const defaultState = new Immutable.List();
 
 export default function projectsReducer (state = defaultState, action) {
 
+  console.log(`projectsReducer`, action.type);
+
   return state;
 }
